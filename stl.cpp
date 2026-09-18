589522c1c91c3938582e70146951f445d11faa70
Abhishek-pytech <itsabhishek.patel29@gmail.com>
2026-09-19 00:14:04 +0530

    use return fun

--- stl.cpp
+++ stl.cpp
@@ -1,13 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-int sum(int a, int b){
-    return a+b;
+int sum(int n1, int n2){
+    return n1+n2;
 }
 int main(){
     int n1,n2;
     cout<< "Enter your first number : ";
+    cin >> n1;
     cout<<"Enter your second number: ";
-    cin >> n1 >> n2;
-    cout<< n1<<endl<<n2;
+    cin>>n2;
+    cout << sum(n1, n2);
     return 0;
 }
