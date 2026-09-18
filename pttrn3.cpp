2df90879bda9d972b658408a8431b3b45cc882ad
Abhishek-pytech <itsabhishek.patel29@gmail.com>
2026-09-18 23:52:14 +0530

    add stl file

--- pttrn3.cpp
+++ pttrn3.cpp
@@ -227,6 +227,13 @@ void print15(int n){
     }
 }
 
+// void print16(int n){
+//     for(int i=0;i<n;i++){
+//         for(int j=1;j<=(n-i-1);j++){
+//             cout<<;
+//         }
+//     }
+// }
 
 
 
