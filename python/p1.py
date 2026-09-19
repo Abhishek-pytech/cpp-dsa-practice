d73bceb09de982c95eb26990d211d826316298c0
Abhishek-pytech <itsabhishek.patel29@gmail.com>
2026-09-19 18:35:23 +0530

    add python folder

---
+++ python/p1.py
@@ -0,0 +1,8 @@
+n1,n2=input("Enter number: ").split()
+if n1==n2:
+    print(f"{n1}and {n2} are equeal")
+elif n1>n2:
+    print(f"{n1} is the greater number than {n2}")
+else:
+    print(f"{n2} is the greater number than {n1}")
+    
