404ccb26802e8f340c2720360e8cc19b832669e5
Abhishek-pytech <itsabhishek.patel29@gmail.com>
2026-09-19 18:55:32 +0530

    fun

--- python/p1.py
+++ python/p1.py
@@ -1,6 +1,8 @@
-num=int(input("Enter a number: "))
+x,y,z=map(int,input("Enter a number: ").split())
 
-if num%5==0 and num%3==0:
-    print("yes")
-else:
-    print("no")
+if x>y and x>z:
+    print(x)
+elif y>x and y>z:
+    print(y)
+elif z>x and z>y:
+    print(z)
