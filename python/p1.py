ff01ef9b88395826e63f2d301d5bd1001c8eea0a
Abhishek-pytech <itsabhishek.patel29@gmail.com>
2026-09-21 18:15:32 +0530

    add

--- python/p1.py
+++ python/p1.py
@@ -1,14 +1,17 @@
-txt="python"
-# for i in txt[::-1]:
-#     print(i)
-feq={}
-count=0
-for ch in txt:
-    if ch  in feq:
-        feq[ch]+=1
-    else:
-        feq[ch]=1
-print(feq)
+# txt="python"
+# # for i in txt[::-1]:
+# #     print(i)
+# feq={}
+# count=0
+# for ch in txt:
+#     if ch  in feq:
+#         feq[ch]+=1
+#     else:
+#         feq[ch]=1
+# print(feq)
    
-    
-    
+n=int((input("enetr number")))  
+for i in range(n):
+    for j in range(n+1):
+        print("*", end=" ")
+    print()
