b981899786ae7b7a967e12569ec190c559406a32
Abhishek-pytech <itsabhishek.patel29@gmail.com>
2026-09-23 00:23:33 +0530

    add

--- python/p1.py
+++ python/p1.py
@@ -1,11 +1,10 @@
 n=int(input("Enter a number: "))
-count=0
+rev=0
 while n>0:
     r=n%10
-    
-    if r%2!=0:
-        count+=1
     n//=10
-        
-print(count)
+    rev=rev*10+r
+    
+print(rev)
+    
     
