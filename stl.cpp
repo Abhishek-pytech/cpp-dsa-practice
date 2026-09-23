910486a70c15c8c9af710c97d9bc5742e7bc0cdd
Abhishek-pytech <itsabhishek.patel29@gmail.com>
2026-09-23 23:26:24 +0530

    pair pair

--- stl.cpp
+++ stl.cpp
@@ -1,9 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
- void explainpair(){
-    pair<int,int> p={1,2};
-    cout<<p.first;
+//  void explainpair(){
+//     pair<int,int> p={1,2};
+//     cout<<p.first;
+
+void explainpair(){
+    pair<int, pair<int, int>> p = {1, {4, 2}};
+    cout<<p.first<<endl<<p.second.second;
+
 
 
  }
