db5a0fe9b457a83e0c6f67664a07513ec2d107a5
Abhishek-pytech <itsabhishek.patel29@gmail.com>
2026-09-23 23:39:19 +0530

    vecotr

--- stl.cpp
+++ stl.cpp
@@ -9,10 +9,15 @@ void explainpair(){
     pair<int, pair<int, int>> p = {1, {4, 2}};
     cout<<p.first<<endl<<p.second.second;
 
-
-
  }
+
+void explainVector(){
+    vector<int>v={};
+    v.push_back(1);
+    for(int value: v)
+    cout<<value;
+}
 int main(){
-    explainpair();
+    explainVector();
    
 }
